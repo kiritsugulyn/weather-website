615ea075b35e60b8e9c6f503ac10e94f088ddfc3
kiritsugulyn <kiritsugulyn@gmail.com>
2020-05-09 20:53:03 +0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Client side JS file is loaded')
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const address = search.value
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
+    fetch(`/weather?address=${address}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error){
+                message1.textContent = data.error
+            }else{
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
+    })
+})
